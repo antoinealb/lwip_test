c4c3ce9976c2225ac4e39e0da23741d1c09a9234
Antoine Albertelli <antoine.albertelli@gmail.com>
2013-12-17 22:46:33 +0100

    Changed time function headers.

--- time.h
+++ time.h
@@ -2,16 +2,16 @@
 #define _TIME_H_
 
 /** Returns the current time, rounded to closest second. */
-int32_t time_get_s(void);
+int time_get_s(void);
 
 /** Returns the current second fraction, rounded to closest second. */
-int32_t time_get_us(void);
+int time_get_us(void);
 
 /** Sets current time.
  * @param [in] s The time, in seconds (rounded to lower integer).
  * @param [in] us The second fraction, in microsecond.
  */
-void time_set(int32_t s, int32_t us);
+void time_set(int s, int us);
 
 
 #endif
