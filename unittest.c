aaacf080c87e0cc51cf5ae204b4735f9835290ec
Antoine Albertelli <antoinea101@gmail.com>
2014-03-16 22:27:22 +0100

    Forgot netconn_close at the end of simple test.

--- unittest.c
+++ unittest.c
@@ -52,11 +52,52 @@ char* simple_test(void) {
     netbuf_data(buf, &data, &len);
     TEST_ASSERT("Data is not echoed correctly", !strcmp(data, test_str));
 
-#ifdef __unix__
-    printf("Test ok.");
-    exit(EXIT_SUCCESS);
-#endif
-    for(;;);
+    netconn_close(conn);
+}
+
+char* fragmented_packet_test(void) {
+    ip_addr_t destination, self_ip;
+    printf("%s()\n", __FUNCTION__);
+    struct netconn *conn;
+    err_t err;
+    struct netbuf *buf;
+    void *data;
+    u16_t len;
+
+    /* Payload longer than MTU, should get split. */
+    char test_str[2001];
+    test_str[0] = 0;
+
+    /* Fills the data pattern. */
+    while (strlen(test_str) < 2000)
+        strcat(test_str, "data");
+
+
+    /* Create a new connection identifier. */
+    conn = netconn_new(NETCONN_TCP);
+
+    /* Sets the device we want to connect to. */
+    IP4_ADDR(&destination, 10, 0, 0, 2);
+    IP4_ADDR(&self_ip, 10, 0, 0, 3);
+
+    /* Bind connection to well known port number 7. */
+    netconn_bind(conn, &self_ip, 1235);
+
+    printf("Connecting...\n");
+    err = netconn_connect(conn, &destination, 1235);
+
+    TEST_ASSERT("TCP connection failed.", err == ERR_OK);
+
+    /* Don't send final \0 */
+    err = netconn_write(conn, test_str, strlen(test_str), NETCONN_NOCOPY);
+
+    TEST_ASSERT("Netconn write failed.\n", err == ERR_OK);
+
+    err = netconn_recv(conn, &buf);
+    TEST_ASSERT("Recv failed.", err == ERR_OK);
+
+    netbuf_data(buf, &data, &len);
+    TEST_ASSERT("Data is not echoed correctly", !strcmp(data, test_str));
 }
 
 void unit_test_run_all(void)
